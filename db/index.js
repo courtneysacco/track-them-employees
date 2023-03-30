42e94804f447f1f6ffb019b3608e902da343cf18
courtney <xoc0urtney14xo@aim.com>
2023-03-30 15:16:52 -0400

    updated most of code

--- db/index.js
+++ db/index.js
@@ -1,53 +1,98 @@
 const connection = require("./connection");
 
-class employeeDB {
-    constructor(connection) {
-        this.connection = connection;
-    }
-    // show employees
-    allEmployees() {
-        return this.connection.promise().query(
-            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
-        );
-    }
-    // create emplyee
-    addEmployee(employee) {
-        return this.connection.promise().query("INSERT INTO employee SET ?", employee);
-    }
-    // update an employees role
-    updateEmployeeRole(employeeId, roleId) {
-        return this.connection.promise().query(
-            "UPDATE employee SET role_id = ? WHERE id = ?",
-            [roleId, employeeId]
-        );
-    }
-    // show managers
-    allManagers(employeeId) {
-        return this.connection.promise().query(
-            "SELECT id, first_name, last_name FROM employee WHERE id != ?",
-            employeeId
-        );
-    }
-    // show roles
-    allRoles() {
-        return this.connection.promise().query(
-            "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
-        );
-    }
-    // create role
-    addRole(role) {
-        return this.connection.promise().query("INSERT INTO role SET ?", role);
-    }
-    // show departments
-    allDepartments() {
-        return this.connection.promise().query(
-            "SELECT department.id, department.name FROM department;"
-        );
-    }
-    // create department
-    addDepartment(department) {
-        return this.connection.promise().query("INSERT INTO department SET ?", department);
-    }
+class DB {
+  constructor(connection) {
+    this.connection = connection;
+  }
+
+  showEmployees() {
+    return this.connection.promise().query(
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+    );
+  }
+
+  showManagers(employeeId) {
+    return this.connection.promise().query(
+      "SELECT id, first_name, last_name FROM employee WHERE id != ?",
+      employeeId
+    );
+  }
+
+  addEmployee(employee) {
+    return this.connection.promise().query("INSERT INTO employee SET ?", employee);
+  }
+
+  removeEmployee(employeeId) {
+    return this.connection.promise().query(
+      "DELETE FROM employee WHERE id = ?",
+      employeeId
+    );
+  }
+
+  updateEmployee(employeeId, roleId) {
+    return this.connection.promise().query(
+      "UPDATE employee SET role_id = ? WHERE id = ?",
+      [roleId, employeeId]
+    );
+  }
+
+  updateManager(employeeId, managerId) {
+    return this.connection.promise().query(
+      "UPDATE employee SET manager_id = ? WHERE id = ?",
+      [managerId, employeeId]
+    );
+  }
+
+  findAllRoles() {
+    return this.connection.promise().query(
+      "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+    );
+  }
+
+  createRole(role) {
+    return this.connection.promise().query("INSERT INTO role SET ?", role);
+  }
+
+  removeRole(roleId) {
+    return this.connection.promise().query("DELETE FROM role WHERE id = ?", roleId);
+  }
+
+  findDepartments() {
+    return this.connection.promise().query(
+      "SELECT department.id, department.name FROM department;"
+    );
+  }
+
+  viewDepartmentSalary() {
+    return this.connection.promise().query(
+      "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;"
+    );
+  }
+
+  createDepartment(department) {
+    return this.connection.promise().query("INSERT INTO department SET ?", department);
+  }
+
+  removeDepartment(departmentId) {
+    return this.connection.promise().query(
+      "DELETE FROM department WHERE id = ?",
+      departmentId
+    );
+  }
+
+  findEmployees(departmentId) {
+    return this.connection.promise().query(
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;",
+      departmentId
+    );
+  }
+
+  findAll(managerId) {
+    return this.connection.promise().query(
+      "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
+      managerId
+    );
+  }
 }
 
-module.exports = new employeeDB(connection);
+module.exports = new DB(connection);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,24 @@
-USE employees;
+use employees;
 
 INSERT INTO department
     (name)
 VALUES
-    ('Human Resources'),
-    ('Marketing'),
-    ('Information Technology'),
-    ('Accounting');
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
-    INSERT INTO role
+INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('HR Manager', 100000, 1),
-    ('HR', 50000, 1),
-    ('Marketing Director', 140000, 2),
-    ('Sales', 65000, 2),
-    ('IT Director', 195000, 3),
-    ('Desktop Support', 100000, 3),
-    ('Account Manager', 110000, 4),
-    ('Accountant', 90000, 4);
+    ('Sales Manager', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Head Engineer', 150000, 2),
+    ('Beginner Engineer', 120000, 2),
+    ('Accounting Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Lead Legal', 250000, 4),
+    ('Lawyer', 190000, 4);
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
@@ -27,7 +27,7 @@ VALUES
     ('Nathan', 'Scott', 2, 1),
     ('Peyton', 'Sawyer', 3, NULL),
     ('Brooke', 'Davis', 4, 3),
-    ('Haley', 'James', 5, 3),
-    ('Karen', 'Roe', 6, NULL),
-    ('Whitey', 'Durhamn', 7, 5),
-    ('Keith', 'Scott', 8, 5)
+    ('Haley', 'James', 5, NULL),
+    ('Karen', 'Roe', 6, 5),
+    ('Whitey', 'Durhamn', 7, NULL),
+    ('Keith', 'Scott', 8, 7)
